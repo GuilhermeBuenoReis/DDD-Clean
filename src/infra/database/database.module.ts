f58a1519f26d7b379b7ab32aba7ccda1a1bd250a
Guilherme Bueno Reis <guilhermebuenoreis.contact@gmail.com>
2025-11-26 17:38:34 -0300

    fix: test

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -6,7 +6,7 @@ import { PrismaAnswerAttachmentRepository } from './prisma/repositories/prisma-a
 import { PrismaAnswerCommentRepository } from './prisma/repositories/prisma-answer-comments-repository';
 import { PrismaAnswerRepository } from './prisma/repositories/prisma-answers-repository';
 import { PrismaQuestionsAttachmentsRepository } from './prisma/repositories/prisma-questions-attachments-repository';
-import { PrismaQuestionsCommentRepository } from './prisma/repositories/prisma-questions-comment';
+import { PrismaQuestionsCommentRepository } from './prisma/repositories/prisma-questions-comment-repository';
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository';
 import { PrismaStudentRepository } from './prisma/repositories/prisma-student-repository';
 
